e844f33218a8f459379b01cbab6e63629f99339c
Mike <michael.reckhaus@h-brs.de>
2010-11-05 14:02:11 +0100

    added ruby

---
+++ sysdep/ruby.mk
@@ -0,0 +1,25 @@
+DEPEND_DEPTH:=   ${DEPEND_DEPTH}+
+RUBY_DEPEND_MK:= ${RUBY_DEPEND_MK}+
+
+RUBY_VERSION=1.8
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=ruby
+endif
+
+ifeq (+,$(RUBY_DEPEND_MK)) 
+
+PREFER.ruby?=	system
+DEPEND_USE+=		ruby
+DEPEND_ABI.ruby?=	ruby>=$(RUBY_VERSION)
+
+SYSTEM_SEARCH.ruby=	\
+	bin/ruby$(RUBY_VERSION) \
+	lib/ruby/$(RUBY_VERSION)/rubyunit.rb
+	
+
+SYSTEM_PKG.Linux.ruby=ruby$(RUBY_VERSION)
+
+endif 
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
