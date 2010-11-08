f58e36c6f97ae913678a9caa64b19c6424d48988
Mike <michael.reckhaus@h-brs.de>
2010-11-08 13:34:00 +0100

    Added rgen makefile

---
+++ rgen/depend.mk
@@ -0,0 +1,24 @@
+# robotpkg depend.mk for:	brics/rgen
+# Created:			Michael Reckhaus on 05 Nov 2010
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+RGEN_DEPEND_MK:= ${RGEN_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		rgen
+endif
+
+ifeq (+,$(RGEN_DEPEND_MK))
+PREFER.rgen?=	robotpkg
+
+DEPEND_USE+=		rgen
+
+DEPEND_ABI.rgen?=	rgen>=0.5
+DEPEND_DIR.rgen?=	../../brics/rgen
+
+SYSTEM_SEARCH.rgen=\
+  include/urg/UrgCtrl.h
+endif
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
