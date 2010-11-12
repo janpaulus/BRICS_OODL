a69f201348de8e4488427e2f2c4f64d8d28253e9
Jan Paulus <jan.paulus@h-brs.de>
2010-11-12 14:21:58 +0100

    added rudeconfig config file reader

---
+++ rudeconfig/depend.mk
@@ -0,0 +1,24 @@
+# robotpkg depend.mk for:	brics/rudeconfig
+# Created:			Jan Paulus on 12 Nov 2010
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+RUDECONFIG_DEPEND_MK:= ${RUDECONFIG_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		rudeconfig
+endif
+
+ifeq (+,$(RUDECONFIG_DEPEND_MK))
+PREFER.rudeconfig?=	robotpkg
+
+DEPEND_USE+=		rudeconfig
+
+DEPEND_ABI.rudeconfig?=	rudeconfig>=2.1
+DEPEND_DIR.rudeconfig?=	../../brics/rudeconfig
+
+SYSTEM_SEARCH.rudeconfig=\
+  include/urg/UrgCtrl.h
+endif
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
