6723fea48b8715901f06f45d6903276b92763640
Jan Paulus <jan.paulus@h-brs.de>
2010-12-02 17:27:10 +0100

    doxygen can be generated by make doc

--- youbot/ProtocolDefinitions.hpp
+++ youbot/ProtocolDefinitions.hpp
@@ -5,6 +5,20 @@
  * Created on November 18, 2010, 4:10 PM
  */
 
+/*! \mainpage BRICS OODL youBot API
+ *
+ * \section intro_sec Introduction
+ *
+ * This is the introduction.
+ *
+ * \section install_sec Installation
+ *
+ * \subsection step1 Step 1: Opening the box
+ *
+ * etc...
+ */
+
+
 #ifndef PROTOCOLDEFINITIONS_HPP
 #define	PROTOCOLDEFINITIONS_HPP
 
