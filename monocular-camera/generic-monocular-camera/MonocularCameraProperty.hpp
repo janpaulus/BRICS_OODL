87059ec22f9ef9f263e86965012a1772c4b77988
Azamat Shakhimardanov <azamat.shakhimardanov@fh-brs.de>
2011-04-06 18:04:44 +0200

    added list of property descriptors

---
+++ monocular-camera/generic-monocular-camera/MonocularCameraProperty.hpp
@@ -0,0 +1,20 @@
+Brightness
+Contrast
+Saturation
+Hue
+White Balance Temperature, Auto
+Gamma
+Gain
+Power Line Frequency
+White Balance Temperature
+Sharpness
+Backlight Compensation
+Exposure, Auto
+Exposure (Absolute)
+Focus (absolute)
+Focus, Auto
+Zoom, Absolute
+frame rate
+Lens Iris
+Shutter time
+Color temperature
